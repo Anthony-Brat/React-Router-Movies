15fc24de9f1980846202ab197a36d9f4b9df066c
Anthony Bratcher <tonybrat74@gmail.com>
2020-07-13 14:26:58 -0700

    part1

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route, Switch, Link } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie'
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -28,7 +30,14 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+        <Route exact path={"/"}>
+          <MovieList movieList={movieList}/>
+        </Route>
+      <Route path={"/movies/:id"}>
+        <Movie params={movieList}/>
+      </Route>
+      </Switch>
     </div>
   );
 };
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const MovieList = props => {
+  const [movies, setMovies] = useState([])
+  useEffect(()=>{
+    const grabMovies = () => {
+      axios
+      .get(`http://localhost:5000/api/movies`)
+
+      .then(response => {
+        setMovies(response.data);
+      })
+    .catch (error => {
+
+    });
+  }
+  grabMovies();
+}, []);
+
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
+      {movies.map(movie => (
+        <Link to ={`/movies/${movie.id}`}>
         <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
